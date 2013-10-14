8120f26685505ca01a08611ef83ea3e2e6e33f8c
Makoto Fukuhara <fukuo33@gmail.com>
2013-10-14 15:21:48 +0900

    initial commit.

---
+++ src/main/scala/fukuo33/FunctionalProgramming.scala
@@ -0,0 +1,12 @@
+package fukuo33
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fukuo33
+ * Date: 2013/10/14
+ * Time: 14:45
+ * To change this template use File | Settings | File Templates.
+ */
+object FunctionalProgramming extends App {
+  println("Hello Functional Programming !!")
+}
